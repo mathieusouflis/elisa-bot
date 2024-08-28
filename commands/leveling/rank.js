6824c405cc2c6b9e8306fe79e39b4509c40a9fa9
Mathieu <spiie.pro@souflis.fr>
2024-08-28 18:03:38 +0200

    Refreshtoken 2month life

--- commands/leveling/rank.js
+++ commands/leveling/rank.js
@@ -1,28 +1,36 @@
-const { SlashCommandBuilder } = require('discord.js');
-const {getDb} = require("../../utils/functions/db");
-const { xpForLevel } = require('../../utils/functions/leveling');
+const { SlashCommandBuilder } = require("discord.js");
+const { getDb } = require("../../utils/functions/db");
+const { xpForLevel } = require("../../utils/functions/leveling");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('rank')
-        .setDescription("Leveling plugin")
-        .addUserOption((option) => 
-            option
-                .setName("user")
-                .setDescription("L'utilisateur que vous souhaitez inspecter.")
-                .setRequired(false)
-        ),
-    async execute(client, interaction) {
+  data: new SlashCommandBuilder()
+    .setName("rank")
+    .setDescription("Leveling plugin")
+    .addUserOption((option) =>
+      option
+        .setName("user")
+        .setDescription("L'utilisateur que vous souhaitez inspecter.")
+        .setRequired(false),
+    ),
+  async execute(client, interaction) {
+    let database = await getDb();
+    let users =
+      database["guilds"][interaction.guild.id.toString()]["leveling"]["users"];
 
-        let database = await getDb()
-        let users = database["guilds"][interaction.guild.id.toString()]["leveling"]["users"]
-        
-        interactionAuthor = interaction.user.id
-        const userOption = interaction.options._hoistedOptions[0] || null
-        const userId = userOption === null ? interactionAuthor : userOption.user.id
-        const userLevel = users[userId]
-        if(userId === interactionAuthor) return await interaction.reply({content: !userLevel ? "Vous n'avez encore jamais envoyé de messages dans le serveur." : `Vous êtes actuellement au rang : ${userLevel["level"]} et avez ${userLevel["xp"]}/${xpForLevel(userLevel["level"] + 1)}xp.` }) // CALCULER L'XP MANQUANT
-        return await interaction.reply({content: !userLevel ? `<@${userId}> n'a encore jamais envoyé de messages dans le serveur.` : `<@${userId}> est actuellement au rang : ${userLevel["level"]} et a ${userLevel["xp"]}/${xpForLevel(userLevel["level"] + 1)}xp.` }) // CALCULER L'XP MANQUANT
-
-    },
+    interactionAuthor = interaction.user.id;
+    const userOption = interaction.options._hoistedOptions[0] || null;
+    const userId = userOption === null ? interactionAuthor : userOption.user.id;
+    const userLevel = users[userId];
+    if (userId === interactionAuthor)
+      return await interaction.reply({
+        content: !userLevel
+          ? "Vous n'avez encore jamais envoyé de messages dans le serveur."
+          : `Vous êtes actuellement au rang : ${userLevel["level"]} et avez ${userLevel["xp"]}/${xpForLevel(userLevel["level"] + 1)}xp.`,
+      }); // CALCULER L'XP MANQUANT
+    return await interaction.reply({
+      content: !userLevel
+        ? `<@${userId}> n'a encore jamais envoyé de messages dans le serveur.`
+        : `<@${userId}> est actuellement au rang : ${userLevel["level"]} et a ${userLevel["xp"]}/${xpForLevel(userLevel["level"] + 1)}xp.`,
+    }); // CALCULER L'XP MANQUANT
+  },
 };
